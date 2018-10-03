d7ce537500d1dd18a3a0bf6203c5de06a2bbcb0d
Akash Guha <akaguha@terpmail.umd.edu>
2018-10-02 22:31:05 -0400

    Bug Fixes

--- app/main.cpp
+++ app/main.cpp
@@ -1,3 +1,17 @@
+/**
+ *  Copyright 2018 Akash Guha
+ *  @file    main.cpp
+ *  @author  Akash Guha(akaguha@terpmail.umd.edu)
+ *  @date    10/02/2018
+ *  @version 1.0
+ *
+ *  @brief ENPM808X, Valgrind Exercise
+ *
+ *  @section DESCRIPTION
+ *
+ *  Main function of AnalogSensor program
+ *
+ */
 #include <iostream>
 #include <AnalogSensor.hpp>
 
@@ -5,7 +19,7 @@ int main()
 {
     AnalogSensor lightSensor(5);
     std::cout << "Averaged sensor reading: " << lightSensor.Read() << std::endl;
-    bool terminator;
+    bool terminator=false; //initializing with an explicit value
     if( terminator )
     {
         std::cout << "DONE" << std::endl;
